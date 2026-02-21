e4ca7a4f392d9df27d6ad1cdec2d5ef3daa6e04c
Prabhat-kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-02-21 17:07:18 +0530

    day3

---
+++ Day3_Hashmap/Anagram.java
@@ -0,0 +1,39 @@
+package Day3_Hashmap;
+
+import java.util.HashMap;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String str1="car";
+        String str2="rca";
+
+    boolean result = IsAnagram(str1, str2);
+
+    if(result){
+        System.out.println("Anagram");
+    } else {
+        System.out.println("Not Anagram");
+    }
+
+    }
+    public static boolean IsAnagram(String str1,String str2 ){
+        if(str1.length()!=str2.length()){
+            return false;
+        }
+
+        HashMap<Character , Integer> map= new HashMap<>();
+        //frequncy of str1
+        for(char c:str1.toCharArray()){
+            map.put(c,map.getOrDefault(c, 0)+1);
+        }
+        for(char c:str2.toCharArray()){
+            if(!map.containsKey(c) || map.get(c)==0){
+                return false;
+            }
+            map.put(c,map.get(c)-1);
+        }
+
+        return true;
+
+    }
+}
