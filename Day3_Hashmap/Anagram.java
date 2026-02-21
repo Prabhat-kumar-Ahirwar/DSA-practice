c6e19f67d7a2e4a3ef9cd888ffd2a0b2ae50dd00
Prabhat-kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-02-21 17:15:25 +0530

    day3

--- Day3_Hashmap/Anagram.java
+++ Day3_Hashmap/Anagram.java
@@ -21,17 +21,16 @@ public class Anagram {
             return false;
         }
 
-        HashMap<Character , Integer> map= new HashMap<>();
-        //frequncy of str1
-        for(char c:str1.toCharArray()){
-            map.put(c,map.getOrDefault(c, 0)+1);
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char c: str1.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
         }
         for(char c:str2.toCharArray()){
             if(!map.containsKey(c) || map.get(c)==0){
                 return false;
             }
-            map.put(c,map.get(c)-1);
-        }
+            map.put(c,map.get(c));
+              }
 
         return true;
 
