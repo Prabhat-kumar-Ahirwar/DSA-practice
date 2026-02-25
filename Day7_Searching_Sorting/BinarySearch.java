a7221e5ed4545eff35994efc962c23b3de4c217d
Prabhat-kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-02-25 17:10:00 +0530

    Day 7

---
+++ Day7_Searching_Sorting/BinarySearch.java
@@ -0,0 +1,34 @@
+package Day7_Searching_Sorting;
+
+public class BinarySearch {
+    
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8};
+        int target = 9;
+        if(!Searching(arr,target)){
+            System.out.println("Target not found");
+        }
+    }
+    
+    public static boolean Searching(int arr[] ,int target){
+        int n =arr.length;
+        int low= 0;
+        int high =n-1;
+        boolean flag = false;
+        while(low<=high){
+             int mid= low+ (high - low )/2; 
+            if(arr[mid]==target){
+                System.out.println("Target found at index : " +mid);
+                return true;
+            }
+            else if(target > arr[mid]){
+                low = mid +1;
+            }
+            else
+            {
+                high = mid-1; 
+            }
+        }
+        return false;
+    }
+}
