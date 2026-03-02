6d971f4ed2132a0a852a9c66e1cdc0c11f089618
Prabhat-kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-03-02 17:35:59 +0530

    Day 11

---
+++ Day11_Srting/Count_Vowel_Consonants.java
@@ -0,0 +1,27 @@
+package Day11_Srting;
+
+public class Count_Vowel_Consonants {
+    public static void main(String[] args) {
+        String str = "aeiou";
+        CountVowelConsonants(str);
+    }
+    public static void CountVowelConsonants(String str){
+        int CountVowel =0;
+        int CountConsonants =0;
+        str = str.toLowerCase();
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch>='a' && ch<='z'){
+                 if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    CountVowel++;
+                    }
+                    else{
+                        CountConsonants++;
+                    }
+            }
+        }
+        System.out.println("Count of Vowel : " + CountVowel + " and "+ "Count of Consonants : "+CountConsonants);
+
+
+    }
+}
