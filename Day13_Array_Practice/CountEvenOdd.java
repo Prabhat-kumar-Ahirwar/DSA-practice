5af7afab63c5954b32e50a639982c795fc14f06f
Prabhat-kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-03-05 17:11:33 +0530

    day 13

---
+++ Day13_Array_Practice/CountEvenOdd.java
@@ -0,0 +1,26 @@
+package Day13_Array_Practice;
+
+public class CountEvenOdd {
+    
+    public static void main(String[] args) {
+        int arr[] ={1,2,3,4,5};
+        CountEO(arr);
+    }
+
+    private static void CountEO(int[] arr) {
+      
+        int Ecount =0 ;
+        int Ocount=0;
+        for(int i=0;i<arr.length;i++){
+            
+            if(arr[i]%2 == 0){
+                Ecount++;
+            }
+            else{
+                Ocount++;
+            }
+        }
+        System.out.println("Even Count : "+ Ecount);
+        System.out.println("Odd Count : "+ Ocount);
+    }
+}
