406cd73e89bcbb5929cb5e644b648708b6f801e6
Prabhat-Kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-05-13 22:49:32 +0530

    BlancedBinary TREE

---
+++ Tree/BalancedBinaryTree.java
@@ -0,0 +1,83 @@
+package Tree;
+// File Name: BalancedBinaryTree.java
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class BalancedBinaryTree {
+
+    // Function to check if tree is balanced
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    // Returns height if balanced
+    // Returns -1 if not balanced
+    private int checkHeight(TreeNode root) {
+
+        // Base case
+        if (root == null) {
+            return 0;
+        }
+
+        // Check left subtree
+        int left = checkHeight(root.left);
+
+        // If left subtree already unbalanced
+        if (left == -1) {
+            return -1;
+        }
+
+        // Check right subtree
+        int right = checkHeight(root.right);
+
+        // If right subtree already unbalanced
+        if (right == -1) {
+            return -1;
+        }
+
+        // Check current node balance
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+
+        // Return height
+        return Math.max(left, right) + 1;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        BalancedBinaryTree tree = new BalancedBinaryTree();
+
+        /*
+                  1
+                 / \
+                2   3
+               / \
+              4   5
+
+           This tree is balanced
+        */
+
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        // Check balanced
+        boolean result = tree.isBalanced(root);
+
+        System.out.println("Is Balanced Tree? " + result);
+    }
+}
