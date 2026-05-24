8f6d8e55f5ee057a017cbf9cc0077ee45129323a
Prabhat-Kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-05-24 23:21:28 +0530

    4 GFG

---
+++ GFG100/MaximumSubarray.java
@@ -0,0 +1,29 @@
+package GFG100;
+public class MaximumSubarray {
+
+    public static int maxSubArray(int[] arr) {
+
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+
+            currentSum += num;
+
+            maxSum = Math.max(maxSum, currentSum);
+
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        System.out.println("Maximum Subarray Sum: " + maxSubArray(arr));
+    }
+}
